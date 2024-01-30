f32c5d3d319f361580290be94c1083ebe096750c
looop5 <xuhui78@huawei.com>
2024-01-30 09:44:50 +0800

    use ASCEND_PATH

--- env.sh
+++ env.sh
@@ -1,9 +1,18 @@
 export TEST_TARGET=910
 
+if [[ -z "${ASCEND_CUSTOM_PATH}" ]]; then
+  if [[ -z "${ASCEND_PATH}" ]]; then
+    export ASCEND_PATH="/usr/local/Ascend"
+  fi
+else
+  export ASCEND_PATH="${ASCEND_CUSTOM_PATH}"
+fi
+
+echo "ASCEND_PATH: ${ASCEND_PATH}"
+
 # auto config. DONOT config directly
 export PY_BIN_PATH=$(which python3.7m)
 export PY_INCLUDE="${PY_BIN_PATH%bin/python3.7m}/include/python3.7m"
 export PYTHONPATH=$(pwd)/python:${PYTHONPATH}
-export PATH=${ASCEND_CUSTOM_PATH}/latest/compiler/ccec_compiler/bin/:${ASCEND_CUSTOM_PATH}/latest/compiler/bin:${PATH}
-export LD_LIBRARY_PATH=${ASCEND_CUSTOM_PATH}/latest/$(uname -m)-linux/lib64:${LD_LIBRARY_PATH}
-#export LD_LIBRARY_PATH=${ASCEND_CUSTOM_PATH}/latest/toolkit/tools/simulator/Ascend910B1/lib:${LD_LIBRARY_PATH}
+export PATH=${ASCEND_PATH}/latest/compiler/ccec_compiler/bin/:${ASCEND_PATH}/latest/compiler/bin:${PATH}
+export LD_LIBRARY_PATH=${ASCEND_PATH}/latest/$(uname -m)-linux/lib64:${LD_LIBRARY_PATH}

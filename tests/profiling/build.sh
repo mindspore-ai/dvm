5dace4ec87707b3e0c9a0da2ad2fb97187f9b173
huangchengnuo <huangchengnuo1@huawei.com>
2024-03-29 15:19:32 +0800

    fix soc bugs and support code trace

--- tests/profiling/build.sh
+++ tests/profiling/build.sh
@@ -8,7 +8,7 @@ LDFLGAS="-L${ASCEND_PATH}/latest/toolkit/tools/simulator/${DVM_SOC_NAME}/lib -lr
 if [ -n "$2" ]; then
   export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64:/usr/local/Ascend/driver/lib64/common:/usr/local/Ascend/driver/lib64/driver:${LD_LIBRARY_PATH}
   export LD_LIBRARY_PATH=${ASCEND_TOOLKIT_HOME}/tools/aml/lib64:${ASCEND_TOOLKIT_HOME}/tools/aml/lib64/plugin:${LD_LIBRARY_PATH}
-  export LD_LIBRARY_PATH=${ASCEND_CUSTOM_PATH}/latest/tools/simulator/${DVM_SOC_NAME}/lib:${LD_LIBRARY_PATH}
+  export LD_LIBRARY_PATH=${ASCEND_TOOLKIT_HOME}/tools/simulator/${DVM_SOC_NAME}/lib:${LD_LIBRARY_PATH}
   export PYTHONPATH=${ASCEND_TOOLKIT_HOME}/python/site-packages:${ASCEND_TOOLKIT_HOME}/opp/built-in/op_impl/ai_core/tbe:$PYTHONPATH
   export PATH=${ASCEND_TOOLKIT_HOME}/bin:${ASCEND_TOOLKIT_HOME}/compiler/ccec_compiler/bin:${ASCEND_TOOLKIT_HOME}/tools/ccec_compiler/bin:$PATH
   export TOOLCHAIN_HOME=${ASCEND_TOOLKIT_HOME}/toolkit
@@ -17,7 +17,7 @@ fi
 
 echo "----------compile----------"
 
-make -C ../../ libdvm.a sim=1 dbg=1
+make -C ../../ libdvm.a sim=1 dbg=1 -j8
 
 g++ ${CFLAGS} ${TEST_FILE}.cc ../../libdvm.a ${LDFLGAS} -o ${TEST_FILE}
 

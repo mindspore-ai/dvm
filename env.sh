125aad0b9148b06304874c7406bb58c80354e541
XinDu <12561213+xindu1024@user.noreply.gitee.com>
2024-07-12 06:19:45 +0000

    !100 find python version automatically * find python version automatically

--- env.sh
+++ env.sh
@@ -9,8 +9,7 @@ fi
 echo "ASCEND_PATH: ${ASCEND_PATH}"
 
 # auto config. DONOT config directly
-export PY_BIN_PATH=$(which python3.7m)
-export PY_INCLUDE="${PY_BIN_PATH%bin/python3.7m}/include/python3.7m"
+export PY_INCLUDE=$(python -c "from sysconfig import get_paths as gp; print(gp()['include'])")
 export PYTHONPATH=$(pwd)/python:${PYTHONPATH}
 export PATH=${ASCEND_PATH}/latest/compiler/ccec_compiler/bin/:${ASCEND_PATH}/latest/compiler/bin:${PATH}
 export LD_LIBRARY_PATH=${ASCEND_PATH}/latest/$(uname -m)-linux/lib64:${LD_LIBRARY_PATH}

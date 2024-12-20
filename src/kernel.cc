f7db88b15d97b20dd51130621a6e0649c73685e8
Gaoxiong <xiong.gao@huawei.com>
2024-12-20 17:54:21 +0800

    [vkernele] bugfix of splitk workspace overflow

--- src/kernel.cc
+++ src/kernel.cc
@@ -43,7 +43,6 @@ static const NDObjectAttr g_obj_attrs[ObjectType::kObjectBulk] = {
   {kGenLoad, true},    // Load
   {kGenStore, false},  // PadStore
   {kGenStore, true},   // SStore
-  {kGenStore, true},   // PeerStore
   {kGenStore, true},   // Store
   {kGenComm, true},    // ReduceScatter
   {kGenComm, true},    // AllGather
--- src/xkernel.cc
+++ src/xkernel.cc
@@ -268,7 +268,7 @@ uint64_t MixKernel::AlignCodeGen() {
     post_fusion_->Normalize();
     if (cube_op_->batch_fold_) {  // Todo: Support BatchMatMul Broadcast
       for (auto op : post_fusion_->objects_) {
-        ASSERT(op->nd_.prod() == cube_op_->output_->nd_.prod())
+        ASSERT(op->nd_.prod() == cube_op_->output_->nd_.prod());
         op->nd_[1] = cube_op_->m_real_;
         op->nd_.resize(2);
       }
@@ -624,6 +624,7 @@ class CubeOptimizer {
     size_t k_tail = dom_->k_real_ % k_stride ? dom_->k_real_ % k_stride : k_stride;
     size_t offset_a = 0;
     size_t offset_b = 0;
+    dom_->output_->type_id_ = DType::kFloat32;
     for (size_t i = 0; i < split_num - 1; ++i) {
       auto op = new CubeOp(dom_->lhs_, dom_->rhs_, dom_->trans_a_, dom_->trans_b_, i == 0 ? dom_->bias_ : nullptr);
       op->NormalizeCube();

fd341462f41342de1dc6949d72008b2f2831c38b
NaCN <huangchengnuo1@huawei.com>
2024-12-24 10:31:49 +0000

    !212 !211fix power negative^{decimal} * !211 fix power negative^{decimal}

--- tests/ut/test_elemwise.py
+++ tests/ut/test_elemwise.py
@@ -165,12 +165,12 @@ def test_mul1(data_type):
 @pytest.mark.parametrize('type', [np.float16, np.float32])
 def test_pow_1(type):
     t = Tester()
-    a = np.array([-2, -3, -4, 0]).astype(type)
-    b = np.array([1, 2, 3, 0]).astype(type)
+    a = np.array([-2, -3, -4, 0, -2, 9]).astype(type)
+    b = np.array([1, 2, 3, 0, 0.5, 0.5]).astype(type)
     x = t.load(a)
     y = t.load(b)
     z = t.binary("Pow", x, y)
-    t.store_expect(z, np.array([-2, 9, -64, 1]))
+    t.store_expect(z, np.array([-2, 9, -64, 1, np.nan, 3]))
     assert(t.run_check())
 
 @pytest.mark.parametrize('type', [np.float16, np.float32])

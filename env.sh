1f15e26a1c9043770649653c7ffa3ea2e47891f6
looop5 <xuhui78@huawei.com>
2025-03-03 04:49:57 +0000

    !253 fix ci ASCEND_PATH * fix ci ASCEND_PATH

--- env.sh
+++ env.sh
@@ -1,12 +1,18 @@
 if [[ -z "${ASCEND_CUSTOM_PATH}" ]]; then
   if [[ -z "${ASCEND_PATH}" ]]; then
-    export ASCEND_PATH="/usr/local/Ascend"
+    if [ -d "/usr/local/Ascend/ascend-toolkit" ]; then
+      export ASCEND_PATH="/usr/local/Ascend/ascend-toolkit"
+    else
+      export ASCEND_PATH="/usr/local/Ascend"
+    fi
   fi
 else
   export ASCEND_PATH="${ASCEND_CUSTOM_PATH}"
 fi
 
 echo "ASCEND_PATH: ${ASCEND_PATH}"
+echo "ls ${ASCEND_PATH}"
+ls ${ASCEND_PATH}
 
 # auto config. DONOT config directly
 export PY_INCLUDE=$(python -c "from sysconfig import get_paths as gp; print(gp()['include'])")

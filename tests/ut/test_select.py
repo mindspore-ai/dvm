c9a78eb7482d018bc51a3a2828385654b8cbefbd
looop5 <xuhui78@huawei.com>
2026-05-30 11:51:59 +0800

    select int64

--- tests/ut/test_select.py
+++ tests/ut/test_select.py
@@ -15,6 +15,7 @@
 
 import pytest
 import numpy as np
+import dvm
 from dvm.tester import Tester
 from tests.mark_utils import arg_mark
 
@@ -87,4 +88,68 @@ def test_select_scalar():
     g = t.load(c)
     l = t.select(g, e, f)
     t.store_expect(l, np.select([c, ~c], [a, b]).astype(np.float16))
+    assert (t.run_check())
+
+
+@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+@pytest.mark.skipif(dvm.Device.arch() == 'AscendC310', reason="C310 temporarily does not support int64 ops")
+def test_select_int64():
+    t = Tester()
+    shape = (13, 131)
+    a = np.random.randint(-(1 << 40), 1 << 40, size=shape, dtype=np.int64)
+    b = np.random.randint(-(1 << 40), 1 << 40, size=shape, dtype=np.int64)
+    c = np.random.randint(-(1 << 40), 1 << 40, size=shape, dtype=np.int64)
+    d = np.random.randint(-(1 << 40), 1 << 40, size=shape, dtype=np.int64)
+    e = t.load(a)
+    f = t.load(b)
+    g = t.load(c)
+    h = t.load(d)
+    i = t.greater(e, f)
+    l = t.select(i, g, h)
+    t.store_expect(l, np.where(a > b, c, d).astype(np.int64))
+    assert (t.run_check())
+
+
+@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+@pytest.mark.skipif(dvm.Device.arch() == 'AscendC310', reason="C310 temporarily does not support int64 ops")
+def test_select_int64_boundary_patterns():
+    t = Tester()
+    cond = np.array([
+        [0, 1, 1 << 32, -(1 << 32)],
+        [(1 << 31) - 1, -1, 1 << 40, -(1 << 40)],
+        [np.iinfo(np.int64).min, np.iinfo(np.int64).max, (1 << 32) | 7, -((1 << 32) | 9)],
+    ], dtype=np.int64)
+    lhs = np.array([
+        [11, -(1 << 33), 1 << 45, -(1 << 45)],
+        [((1 << 32) | 5), -((1 << 32) | 6), np.iinfo(np.int64).min, np.iinfo(np.int64).max],
+        [((1 << 48) | 0x1234), -((1 << 48) | 0x5678), (1 << 40) | 0x55, -((1 << 40) | 0x66)],
+    ], dtype=np.int64)
+    rhs = np.array([
+        [-11, 1 << 33, -(1 << 44), 1 << 44],
+        [-((1 << 32) | 15), (1 << 32) | 16, 17, -18],
+        [-(1 << 47), 1 << 47, -((1 << 39) | 0x77), (1 << 39) | 0x88],
+    ], dtype=np.int64)
+    x = t.load(cond)
+    y = t.load(lhs)
+    z = t.load(rhs)
+    out = t.select(x, y, z)
+    t.store_expect(out, np.where(cond != 0, lhs, rhs).astype(np.int64))
+    assert (t.run_check())
+
+
+@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+@pytest.mark.skipif(dvm.Device.arch() == 'AscendC310', reason="C310 temporarily does not support int64 ops")
+def test_select_int64_broadcast():
+    t = Tester()
+    a = np.random.randint(-(1 << 40), 1 << 40, size=(1, 10, 1, 1, 50), dtype=np.int64)
+    b = np.random.randint(-(1 << 40), 1 << 40, size=(1, 1, 20, 30, 1), dtype=np.int64)
+    c = np.random.randint(-(1 << 40), 1 << 40, size=(7, 1, 1, 1, 50), dtype=np.int64)
+    d = np.random.randint(-(1 << 40), 1 << 40, size=(1, 1, 1, 30, 1), dtype=np.int64)
+    e = t.load(a)
+    f = t.load(b)
+    g = t.load(c)
+    h = t.load(d)
+    i = t.greater(e, f)
+    l = t.select(i, g, h)
+    t.store_expect(l, np.where(a > b, c, d).astype(np.int64))
     assert (t.run_check())
